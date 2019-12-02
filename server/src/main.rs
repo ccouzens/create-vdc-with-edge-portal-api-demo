ac823bf597c262e48f4a32e2ee6e5d93669e04a5
Chris Couzens <ccouzens@gmail.com>
2019-12-02 21:17:42 +0000

    Proxy the Portal
    It doesn't do anything to make the cookies accessible yet, which was the
    whole point of writing a custom proxy.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,8 @@
 use futures::future;
-use futures::Stream;
 use hyper::rt::Future;
 use hyper::service::service_fn;
+use hyper::StatusCode;
 use hyper::{Body, Client, Request, Response, Server};
-use hyper::{Method, StatusCode};
 use hyper_tls::HttpsConnector;
 
 type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
@@ -11,48 +10,36 @@ type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send
 fn echo() -> impl Fn(Request<Body>) -> BoxFut {
     let https = HttpsConnector::new(4).expect("TLS initialization failed");
     let client = Client::builder().build::<_, hyper::Body>(https);
-    let uri: http::Uri = "http://localhost:3001/".parse().unwrap();
 
     move |req: Request<Body>| -> BoxFut {
         let mut response = Response::new(Body::empty());
+        let path = req.uri().path();
+        let path_prefix = "/proxy/portal-skyscapecloud-com/";
+        if path.starts_with(path_prefix) {
+            let request = Request::builder()
+                .method(req.method())
+                .uri(format!(
+                    "https://portal.skyscapecloud.com/{}",
+                    path.get(path_prefix.len()..path.len()).unwrap()
+                ))
+                .body(req.into_body())
+                .expect("request builder");
 
-        match (req.method(), req.uri().path()) {
-            (&Method::GET, "/") => {
-                *response.body_mut() = Body::from("Try POSTing data to /echo");
-            }
-            (&Method::POST, "/echo") => {
-                *response.body_mut() = req.into_body();
-            }
-            (&Method::POST, "/echo/uppercase") => {
-                let mapping = req.into_body().map(|chunk| {
-                    chunk
-                        .iter()
-                        .map(|byte| byte.to_ascii_uppercase())
-                        .collect::<Vec<u8>>()
-                });
-                *response.body_mut() = Body::wrap_stream(mapping);
-            }
-            (&Method::POST, "/echo/reverse") => {
-                let reversed = req.into_body().concat2().map(move |chunk| {
-                    let body = chunk.iter().rev().cloned().collect::<Vec<u8>>();
-
-                    *response.body_mut() = Body::from(body);
-                    response
-                });
-                return Box::new(reversed);
-            }
-            (&Method::GET, "/echo/vcloud") => {
-                return Box::new(client.get(uri.clone()).then(|res| {
-                    *response.body_mut() = match res {
-                        Ok(res) => Body::wrap_stream(res.into_body()),
-                        Err(err) => Body::from(format!("Error: {}", err)),
-                    };
-                    Ok(response)
-                }))
-            }
-            _ => {
-                *response.status_mut() = StatusCode::NOT_FOUND;
-            }
+            return Box::new(client.request(request).then(|response_result| {
+                *response.body_mut() = match response_result {
+                    Ok(res) => {
+                        let (parts, body) = res.into_parts();
+                        *response.headers_mut() = parts.headers;
+                        *response.status_mut() = parts.status;
+                        *response.version_mut() = parts.version;
+                        Body::wrap_stream(body)
+                    },
+                    Err(err) => Body::from(format!("Error: {}", err)),
+                };
+                Ok(response)
+            }));
+        } else {
+            *response.status_mut() = StatusCode::NOT_FOUND;
         };
 
         Box::new(future::ok(response))
