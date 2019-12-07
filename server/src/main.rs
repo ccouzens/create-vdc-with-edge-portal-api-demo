efd6538700c9573497fae39aa69772439457546c
Chris Couzens <ccouzens@gmail.com>
2019-12-07 22:31:23 +0000

    Remove unwraps from server code
    This should help ensure it is robust

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,51 +7,66 @@ use hyper_tls::HttpsConnector;
 
 type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
+fn split_path(path: &str) -> Option<[&str; 4]> {
+    let mut path_parts = path.splitn(4, '/');
+    Some([
+        path_parts.next()?,
+        path_parts.next()?,
+        path_parts.next()?,
+        path_parts.next()?,
+    ])
+}
+
 fn echo() -> impl Fn(Request<Body>) -> BoxFut {
     let https = HttpsConnector::new(4).expect("TLS initialization failed");
     let client = Client::builder().build::<_, hyper::Body>(https);
 
-    move |req: Request<Body>| -> BoxFut {
-        let mut response = Response::new(Body::empty());
-        let path = req.uri().path();
-        let path_prefix = "/proxy/portal-skyscapecloud-com/";
-        if path.starts_with(path_prefix) {
-            let request = Request::builder()
-                .method(req.method())
-                .uri(format!(
-                    "https://portal.skyscapecloud.com/{}",
-                    path.get(path_prefix.len()..path.len()).unwrap()
-                ))
-                .body(req.into_body())
-                .expect("request builder");
-
-            return Box::new(client.request(request).then(|response_result| {
-                *response.body_mut() = match response_result {
-                    Ok(res) => {
-                        let (parts, body) = res.into_parts();
-                        *response.headers_mut() = parts.headers;
-                        *response.status_mut() = parts.status;
-                        *response.version_mut() = parts.version;
-                        Body::wrap_stream(body)
-                    },
-                    Err(err) => Body::from(format!("Error: {}", err)),
-                };
-                Ok(response)
-            }));
-        } else {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        };
-
-        Box::new(future::ok(response))
+    move |server_req: Request<Body>| -> BoxFut {
+        let mut server_response = Response::new(Body::empty());
+        match split_path(server_req.uri().path()) {
+            Some(["", "proxy", "portal-skyscapecloud-com", path]) => {
+                match Request::builder()
+                    .method(server_req.method())
+                    .uri(format!("https://portal.skyscapecloud.com/{}", path))
+                    .body(server_req.into_body())
+                {
+                    Ok(client_req) => {
+                        Box::new(client.request(client_req).then(|client_response_result| {
+                            *server_response.body_mut() = match client_response_result {
+                                Ok(client_response) => {
+                                    let (parts, body) = client_response.into_parts();
+                                    *server_response.headers_mut() = parts.headers;
+                                    *server_response.status_mut() = parts.status;
+                                    *server_response.version_mut() = parts.version;
+                                    Body::wrap_stream(body)
+                                }
+                                Err(err) => Body::from(format!("Error: {}", err)),
+                            };
+                            Ok(server_response)
+                        }))
+                    }
+                    Err(err) => {
+                        *server_response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+                        *server_response.body_mut() = Body::from(format!("Error: {}", err));
+                        Box::new(future::ok(server_response))
+                    }
+                }
+            }
+            _ => {
+                *server_response.status_mut() = StatusCode::NOT_FOUND;
+                Box::new(future::ok(server_response))
+            }
+        }
     }
 }
 
-fn main() {
+fn main() -> Result<(), hyper::error::Error> {
     let addr = ([127, 0, 0, 1], 3000).into();
 
-    let server = Server::bind(&addr)
+    let server = Server::try_bind(&addr)?
         .serve(|| service_fn(echo()))
         .map_err(|e| eprintln!("server error: {}", e));
 
     hyper::rt::run(server);
+    Ok(())
 }
